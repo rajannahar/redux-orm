ed14d69e7b01374c305103ac1c025db9ad7d758e
rajannahar <R_N101@msn.com>
2017-03-30 16:49:56 +0100

    dummy data

--- src/models.js
+++ src/models.js
@@ -9,9 +9,47 @@ import {
 } from './actionTypes';
 
 
-export class Todo extends Model {} 
+export class Todo extends Model {
+	static reducer(action, Todo, session)  {
+		const { payload, type } = action;
+		switch (type) {
+			case CREATE_TODO:
+				// Payload includes a comma-delimited string
+				// of tags, corresponding to the `name` property
+				// of Tag, which is also its `idAttribute`.
+				const tagIds = action.payload.tag.split(',').map(str => str.trim());
 
-static reducer(action, Todo, session)
+				// You can pass an array of ids for many-to-many relations.
+				// `redux-orm` will create the m2m rows automatically.
+				const props = Object.assign({}, payload, { tags: tagIds });
+
+				Todo.create(props);
+				break;
+
+			case MARK_DONE:
+				// withId returns a Model instance.
+				// Assignment doesn't mutate Model instances.
+				Todo.withId(payload).set('done', true);
+				break;
+
+			case DELETE_TODO;
+				Todo.withId(payload).set('done', true);
+				break;
+
+			case ADD_TAG_TO_TODO:
+				Todo.withId(payload.todo).tags.add(payload.tag);
+				break;
+
+			case REMOVE_TAG_FROM_TODO:
+				Todo.withId(payload.todo).tags.remove(payload.tag);
+				break;
+		}
+
+		// This call is optional. If the reducer returns 'undefined',
+		// Redux-ORM will call getNextState for you.
+		// return Todo.getNextState(); -> this is OLD, 0.8
+	}
+}
 
 Todo.modelName = 'Todo';
 Todo.fields = {
